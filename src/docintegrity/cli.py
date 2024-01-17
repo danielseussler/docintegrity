8a83c6c445d7cc39084da8fbbae68f4c6989ba9b
danielseussler <daniel.seussler@gmail.com>
2024-01-17 20:20:01 +0100

    add dependency

--- src/docintegrity/cli.py
+++ src/docintegrity/cli.py
@@ -4,8 +4,8 @@ from docintegrity.core import find_duplicates
 
 
 @click.command()
-@click.option("--folder-path", prompt="Document Folder", help="Path to the folder that includes the docx files.")
-@click.option("--output-path", prompt="Output Folder", help="Path to the folder where the results should be saved.")
+@click.option("--folder-path", prompt="Document Folder", help="Path to the folder that includes the docx files. Note, the path should be provided without quotation marks.")
+@click.option("--output-path", prompt="Output Folder", help="Path to the folder where the results should be saved. Note, the path should be provided without quotation marks.")
 @click.option(
     "--distance-threshold",
     prompt="Threshold for sentence similarity",
@@ -15,7 +15,7 @@ from docintegrity.core import find_duplicates
 )
 @click.option(
     "--model-name",
-    prompt="Embedding model used",
+    prompt="Embedding model name",
     default="glove-wiki-gigaword-100",
     help="The embedding model used to embedd the sentences in the documents.",
 )
